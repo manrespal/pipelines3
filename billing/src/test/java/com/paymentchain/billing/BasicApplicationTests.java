b861881c4236e3c06f3a8684c800318480be3a4f
manrespal <manrespal@gmali.com>
2023-05-20 01:22:27 -0400

    cuarto cambio

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -115,3 +115,4 @@ public class BasicApplicationTests {
 }/** nuevo cambios realizados wooujjju */
 /** Prueba 20/05/2023 Luis Vasquez */
 /** Prueba 20/05/2023 Luis Vasquez Vera */
+/** Prueba 20/05/2023 Luis Vasquez Vera 3*/
